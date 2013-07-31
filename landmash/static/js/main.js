4db627aeedb5daa9fa36d9327fe9fd73b11ea943
Olivia Haas <livhaas@gmail.com>
2013-07-31 10:16:35 -0400

    pushing some major #metasteez

--- landmash/static/js/main.js
+++ landmash/static/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 
-    
+    // alert('hi');
+    // var date = $('.todaysdate').text();
+
+    var now = moment().format('dddd, MMMM DD' );
+
+    $('.todaysdate').html(now);
+
+
 });
